e3f725cae6bf4867eb101853d0eb434510275667
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2023-10-08 15:09:36 -0300

    Add choose directory

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from os import getenv
+
 import flet as ft
+
+HOME = f'{getenv("HOME")}/'
+
+
+def add_to_path():
+    pass
 
 
 def main(page: ft.Page):
@@ -7,10 +15,22 @@ def main(page: ft.Page):
     page.window_max_width = 500
     page.window_max_height = 200
 
+    def add_to_field(e: ft.FilePickerResultEvent):
+        directory_path.value = e.path
+        page.update()
+
+    directory_path = ft.TextField()
+    folder_picker = ft.FilePicker(on_result=add_to_field)
+
+    page.add(folder_picker)
     page.add(
         ft.Row(
-            [ft.Text('Pasta: ', size=24), ft.TextField(),
-             ft.IconButton(icon=ft.icons.FOLDER, icon_size=40)]
+            [ft.Text('Pasta: ', size=24), directory_path,
+             ft.IconButton(icon=ft.icons.FOLDER, icon_size=40,
+                           on_click=lambda _: folder_picker.get_directory_path(
+                               initial_directory=HOME,
+                               dialog_title='Escolha a pasta para adicionar'
+                           ))]
         ),
         ft.Row(
             [ft.ElevatedButton(content=ft.Text('Adicionar ao PATH', size=24))],
