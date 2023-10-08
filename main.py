71e66a77d33531298d5df2a63ca0702c3f0a4a4d
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2023-10-08 14:50:16 -0300

    Layout pronto

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import flet as ft
 
+
 def main(page: ft.Page):
     page.title = 'Add to Path'
-    page.window_max_height
-    page.window_max_width
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.window_max_width = 500
+    page.window_max_height = 200
+
+    page.add(
+        ft.Row(
+            [ft.Text('Pasta: ', size=24), ft.TextField(),
+             ft.IconButton(icon=ft.icons.FOLDER, icon_size=40)]
+        ),
+        ft.Row(
+            [ft.ElevatedButton(content=ft.Text('Adicionar ao PATH', size=24))],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
+
+    page.padding = 10
+    page.update()
 
 
 if __name__ == '__main__':
